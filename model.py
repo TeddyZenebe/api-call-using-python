ed06b0af0642cfb7361701427cb1cd9a9391451e
Teddy Zenebe <tzenebe@espassociates.com>
2020-08-24 16:59:28 -0400

    tie with  sqlite database

--- model.py
+++ model.py
@@ -1,17 +1,30 @@
 import sqlite3
+from sqlite3 import Error
 import requests
 import xml.etree.ElementTree as ET
+#in case if we need error report---for now I better keep with out using this function but good stuff
+def create_connection(db_file):
+   
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
 
 conn = sqlite3.connect('rarr.db')
 
 db = conn.cursor()
 
-#db.execute("create Table sensorData(site_id text, sensor_id text, data_time text, data_valu real)")
-url = 'https://meckgisdev.mecklenburgcountync.gov/api/contrail?method=GetSensorData&system=c57f3913-ac01-4aa7-b633-e8311f45f74a&class=20'
+#db.execute("create Table sensorData(site_id text, sensor_id text, data_time text, stage(ft) real)")
+url = 'https://meckgisdev.mecklenburgcountync.gov/api/contrail?method=GetSensorData&system=c57f3913-ac01-4aa7-b633-e8311f45f74a&tz=ET&class=20'
 
 res = requests.get(url)
 sensorDatas = res.text
 row = ET.fromstring(sensorDatas)
+sql = ''' INSERT INTO sensorData(site_id, sensor_id, data_time, stage_ft)
+              VALUES(?,?,?,?) '''
 for x in row[0][0].findall('row'):
     if x.find('site_id').text:
         site_id = x.find('site_id').text
@@ -26,12 +39,15 @@ for x in row[0][0].findall('row'):
     else:
         data_time = x.find('data_time').text
     if x.find('data_value').tag:
-        data_value = x.find('data_value').text
+        stage = x.find('data_value').text
     else:
-        data_value = None
+        stage = None
     #text = x.find('data_time')
-    print(site_id,sensor_id, data_time, data_value )
-
+    data = (site_id, sensor_id, data_time, stage)
+    db.execute(sql, data)
+    conn.commit()
+    print(site_id,sensor_id, data_time, stage)
+    
 #db.execute("select * from sensorData")
 
 #conn.commit()
